a87215517a59ea4d4527f83d1532421e8e998ad4
Steph Hoel <steph.hoel@gmail.com>
2024-09-14 21:27:41 -0300

    wip

---
+++ tests/UnitTests/Controller/Attendees/RegisterAttendeeOnEventTest.cs
@@ -0,0 +1,43 @@
+﻿using Api.Controllers;
+using Application.UseCases.Events;
+using Communication.Requests;
+using Communication.Responses;
+using Domain.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using UnitTests.FakeObjects;
+using Xunit;
+
+namespace UnitTests.Controller.Attendees;
+
+public class RegisterAttendeeOnEventTest
+{
+    private readonly RegisterAttendeeOnEventUseCase _useCase;
+
+    public RegisterAttendeeOnEventTest()
+    {
+        var iAttendeeRepository = new Mock<IAttendeeRepository>();
+        _useCase = new RegisterAttendeeOnEventUseCase(iAttendeeRepository.Object);
+    }
+
+    [Fact]
+    public void RegisterAttendeeOnEvent_ValidRequest_ShouldReturn_CreatedResult()
+    {
+        // Arrange
+        var eventId = Guid.NewGuid();
+        var request = FakeRequestRegisterEventJson.Generate();
+        var response = FakeResponseRegisteredJson.Generate();
+
+
+        var controller = new AttendeesController();
+
+        // Act
+        var result = controller.RegisterAttendeeOnEvent(_useCase, eventId, request);
+
+
+        // Assert
+        result.Should().BeOfType<CreatedResult>();
+    }
+}
---
+++ tests/UnitTests/FakeObjects/FakeResponseRegisteredJson.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Communication.Responses;
+
+namespace UnitTests.FakeObjects;
+
+public class FakeResponseRegisteredJson
+{
+    public static ResponseRegisteredJson Generate()
+    {
+        return new Faker<ResponseRegisteredJson>()
+                .RuleFor(a => a.Id, f => f.Random.Guid());
+    }
+}
